3299f6ef2f20198076e727506b25eb57fe197731
Baptiste Trautmann <baptistetrautmann@hotmail.fr>
2023-04-12 15:15:22 +0200

    corrigé

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+function versCelsius(fahrenheit) {
+  return (fahrenheit-32) / 1.8;
+}
+
+function versFahrenheit(celsius) {
+  return (celsius*1.8) + 32;
+}
+
+function VerdictEbullition({celsius}){
+
+  if(celsius >= 100) {
+    return <div className='alert alert-success'>L'eau bout</div>
+  } else {
+    return <div className='alert alert-info'>L'eau ne bout pas</div>
+  }
+}
+
+
+function ChampConversion({temperature, label, setTemperature}){
+
+  function gererChangement(evenement) {
+    if(label === "fahrenheit") {
+      setTemperature(versCelsius(evenement.target.value));
+    } else {
+      setTemperature(evenement.target.value);
+    }
+  }
+
+  return <div>
+    <label htmlFor={label}>Température en {label} </label>
+    <input id={label} type="number" value={temperature} onChange={gererChangement}/>
+  </div>
+}
+
+
+function Convertisseur(){
+  var [temperature, setTemperature ] = useState(50);
+
+  return <div>
+    <ChampConversion temperature={temperature} setTemperature={setTemperature} label="celsius" />
+    <ChampConversion temperature={versFahrenheit(temperature)} label="fahrenheit" setTemperature={setTemperature} />
+    <VerdictEbullition celsius={temperature} />
+    </div>
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Convertisseur />
     </div>
   );
 }
